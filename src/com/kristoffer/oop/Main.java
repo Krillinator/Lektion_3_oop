e5c272418a9397f76aa429b2f81c88187324c771
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2023-09-18 11:55:55 +0200

    new printline feature

--- src/com/kristoffer/oop/Main.java
+++ src/com/kristoffer/oop/Main.java
@@ -6,17 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        // new Comment
-
-        // Git test
-
+        System.out.println("Is this the first COMMIT?!");
 
         // Instantiate Object
         Student benny = new Student("Benny", 15, true);
         Student frida = new Student("Frida", 22, false);
 
-        System.out.println(benny.name);
+
         System.out.println(benny.age);
         System.out.println(benny.isTired);
         if (benny.isTired) {
